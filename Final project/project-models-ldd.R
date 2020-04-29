85d4c348a787a5b36cd51e86463d68d4f9918901
DongdongLi <59519674+Anthony-27@users.noreply.github.com>
2020-04-29 00:58:54 -0400

    support models

--- Final project/project-models-ldd.R
+++ Final project/project-models-ldd.R
@@ -7,7 +7,6 @@ is.factor(hate_crime$state)
 levels(hate_crime$state)
 
 set.seed(42)
-# spam_idx = sample(nrow(spam), round(nrow(spam) / 2))
 hate_crime_idx = sample(nrow(hate_crime), round(nrow(hate_crime)/2))
 hate_crime_trn = hate_crime[hate_crime_idx, ]
 hate_crime_tst = hate_crime[-hate_crime_idx, ]
@@ -24,7 +23,79 @@ mean(ifelse(predict(fit_selected) > 0, "hate", "nonhate") != hate_crime$state)
 mean(ifelse(predict(fit_additive) > 0, "hate", "nonhate") != hate_crime$state)
 mean(ifelse(predict(fit_over) > 0, "hate", "nonhate") != hate_crime$state)
 
+fit_caps_100 <- c()
+fit_selected_100 <- c()
+fit_additive_100 <- c()
+fit_over_100 <- c()
+
+# for loop for 100 fold 
+for (i in 1:100) {
+  #library(boot)
+  #set.seed(1)
+  caps_100 <- cv.glm(hate_crime_trn, fit_caps, K = 5)$delta[1]
+  selected_100 <- cv.glm(hate_crime_trn, fit_selected, K = 5)$delta[1]
+  additive_100 <- cv.glm(hate_crime_trn, fit_additive, K = 5)$delta[1]
+  over_100 <- cv.glm(hate_crime_trn, fit_over, K = 5)$delta[1]
+  
+  
+  fit_caps_100 <- append(fit_caps_100,caps_100)
+  fit_selected_100 <- append(fit_selected_100,selected_100)
+  fit_additive_100 <-append(fit_additive_100,additive_100)
+  fit_over_100 <- append(fit_over_100,over_100)
+}
+mean(fit_caps_100)
+mean(fit_selected_100)
+mean(fit_additive_100)
+mean(fit_over_100)
 
 
+make_conf_mat = function(predicted, actual) {
+  table(predicted = predicted, actual = actual)
+}
+
+
+## fit_additive
+hate_crime_pred = ifelse(predict(fit_additive, hate_crime_tst) > 0,
+                       "hate",
+                       "nonhate")
+hate_crime_pred = ifelse(predict(fit_additive, hate_crime_tst, type = "response") > 0.5,
+                       "hate",
+                       "nonhate")
+
+
+(conf_mat_50 = make_conf_mat(predicted = hate_crime_pred, actual = hate_crime_tst$state))
+table(hate_crime_tst$state) / nrow(hate_crime_tst)
+
+## fit_selected
+hate_crime_pred = ifelse(predict(fit_selected, hate_crime_tst) > 0,
+                       "hate",
+                       "nonhate")
+hate_crime_pred = ifelse(predict(fit_selected, hate_crime_tst, type = "response") > 0.5,
+                       "hate",
+                       "nonhate")
+
+(conf_mat_50 = make_conf_mat(predicted = hate_crime_pred, actual = hate_crime_tst$state))
+table(hate_crime_tst$state) / nrow(hate_crime_tst)
+
+## fit_caps
+hate_crime_pred = ifelse(predict(fit_caps, hate_crime_tst) > 0,
+                       "hate",
+                       "nonhate")
+hate_crime_pred = ifelse(predict(fit_caps, hate_crime_tst, type = "response") > 0.5,
+                       "hate",
+                       "nonhate")
+
+(conf_mat_50 = make_conf_mat(predicted = hate_crime_pred, actual = hate_crime_tst$state))
+table(hate_crime_tst$state) / nrow(hate_crime_tst)
+## fit_over
+hate_crime_pred = ifelse(predict(fit_over, hate_crime_tst) > 0,
+                       "hate",
+                       "nonhate")
+hate_crime_pred = ifelse(predict(fit_over, hate_crime_tst, type = "response") > 0.5,
+                       "hate",
+                       "nonhate")
+
+(conf_mat_50 = make_conf_mat(predicted = hate_crime_pred, actual = hate_crime_tst$state))
+table(hate_crime_tst$state) / nrow(hate_crime_tst)
 
 
